329e23b67e813ca909e82f04c3d905c7ec354750
Khamraj Rohit <khamraj@kaisei.app>
2019-12-07 21:04:55 -0500

    Heap for Pathfinder

---
+++ Assets/Scripts/Data Store/Heap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Heap<T> where T : IComparable<T>
+{
+	public int Count { get; private set; }
+
+	private readonly T[] _heap;
+	private readonly HashSet<T> _hashSet;
+
+	public Heap(int maxSize = 512)
+	{
+		_heap = new T[maxSize];
+		_hashSet = new HashSet<T>();
+	}
+
+	public T RemoveRoot()
+	{
+		_hashSet.Remove(_heap[0]);
+		return _heap[0];
+	}
+
+	public void AddChild(T child)
+	{
+		Count++;
+		_hashSet.Add(child);
+	}
+
+	public bool Contails(T child)
+	{
+		return false;
+	}
+
+}
