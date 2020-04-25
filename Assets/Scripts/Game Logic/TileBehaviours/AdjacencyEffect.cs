b359f552578b8afbc7a7089a21e5933ddfcee2a1
Khamraj Rohit <khamraj@kaisei.app>
2020-04-25 05:20:10 -0400

    Ui Tweaks

---
+++ Assets/Scripts/Game Logic/TileBehaviours/AdjacencyEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Adjacency Effects/Basic")]
+public class AdjacencyEffects : ScriptableObject
+{
+	public virtual List<string> GetAdjacencyEffectsString(BuildingTileEntity building, Tile[] neighbors)
+	{
+		var effects = new List<string>();
+
+		return effects;
+	}
+
+	public virtual void ApplyEffects(BuildingTileEntity building, Tile[] neighbors)
+	{
+
+	}
+}
