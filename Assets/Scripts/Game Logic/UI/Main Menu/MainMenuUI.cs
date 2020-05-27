9e831452f8abd5419b5e2e300e2edb7aeb300ae6
Khamraj Rohit <khamraj@kaisei.app>
2020-05-27 11:03:43 -0400

    Placeholder Main Menu implemented

---
+++ Assets/Scripts/Game Logic/UI/Main Menu/MainMenuUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+	public UIObjectButton playButton;
+	public UIObjectButton optionsButton;
+	public UIObjectButton quitButton;
+
+	public GameObject loadingSplash;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		loadingSplash.SetActive(false);
+		playButton.OnClick += () =>
+		{
+			loadingSplash.SetActive(true);
+			gameObject.SetActive(false);
+			SceneManager.LoadScene(1);
+		};
+
+		quitButton.OnClick += Application.Quit;
+	}
+}
---
+++ Assets/Scripts/Game Logic/UI/Main Menu/UIObjectButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using TMPro;
+
+using UnityEngine;
+
+public class UIObjectButton : MonoBehaviour
+{
+	public event Action OnClick;
+
+	public MeshRenderer buttonMesh;
+	public TMP_Text text;
+	public Color normalColor;
+	public Color hoverColor;
+
+	private void OnValidate()
+	{
+		if(text != null)
+			text.color = normalColor;
+	}
+
+	private void OnMouseUp()
+	{
+		OnClick?.Invoke();
+	}
+
+	private void OnMouseEnter()
+	{
+		text.color = hoverColor;
+	}
+
+	private void OnMouseExit()
+	{
+		text.color = normalColor;
+	}
+}
