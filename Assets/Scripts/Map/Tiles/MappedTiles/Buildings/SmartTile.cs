73fdefcb049547bc868942ca8648f0431183ae8d
Khamraj Rohit <khamraj@kaisei.app>
2020-07-10 07:38:01 -0400

    Started work on Wall Tiles

---
+++ Assets/Scripts/Map/Tiles/MappedTiles/Buildings/SmartTile.cs
@@ -0,0 +1,48 @@
+﻿using Amatsugu.Phos.TileEntities;
+
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+
+using UnityEngine;
+
+namespace Amatsugu.Phos.Tiles
+{
+	public class SmartTile : Tile
+	{
+		public SmartTile(HexCoords coords, float height, Map map, TileEntity tInfo = null) : base(coords, height, map, tInfo)
+		{
+
+		}
+
+		public override void Destroy()
+		{
+			base.Destroy();
+		}
+
+		public override void OnHeightChanged()
+		{
+			base.OnHeightChanged();
+		}
+
+		public override void OnHide()
+		{
+			base.OnHide();
+		}
+
+		public override void OnShow()
+		{
+			base.OnShow();
+		}
+
+		public override Entity Render()
+		{
+			return base.Render();
+		}
+
+		public override void TileUpdated(Tile src, TileUpdateType updateType)
+		{
+			base.TileUpdated(src, updateType);
+		}
+	}
+}
---
+++ Assets/Scripts/Map/Tiles/TileInfo/Buildings/SmartTileEntity.cs
@@ -0,0 +1,21 @@
+﻿
+using Amatsugu.Phos.Tiles;
+
+using UnityEngine;
+
+namespace Amatsugu.Phos.TileEntities
+{
+	[CreateAssetMenu(menuName = "Map Asset/Tile/Connected Tile")]
+	public class SmartTileEntity : TileEntity
+	{
+		public MeshEntityRotatable connectionMesh;
+		public MeshEntityRotatable vertexMesh;
+		public bool alwaysShowVertex;
+		public bool connectToSelf = false;
+
+		public override Tile CreateTile(Map map, HexCoords pos, float height)
+		{
+			return new SmartTile(pos, height, map, this);
+		}
+	}
+}
