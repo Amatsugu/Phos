fc93d8f0529a00b2ebc52532deff74af2ed8c446
Khamraj Rohit <khamraj@kaisei.app>
2021-04-24 21:27:24 -0400

    Added Solar Panel Model and Power Station Model

---
+++ Assets/Scripts/Game Logic/Systems/Effects/ConduitLineRendererSystem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using Unity.Entities;
+
+using UnityEngine;
+
+namespace Amatsugu.Phos
+{
+	public class ConduitLineRendererSystem : ComponentSystem
+	{
+
+
+		protected override void OnUpdate()
+		{
+		}
+	}
+}
